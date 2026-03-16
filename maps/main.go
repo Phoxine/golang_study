1bd8876651f625845fd64e52b05dad2fe9d4a3a5
Peter Chao <kiraychao@gmail.com>
2026-03-16 10:22:36 +0800

    add map example

---
+++ maps/main.go
@@ -0,0 +1,45 @@
+package main
+
+import(
+	"fmt"
+)
+
+type Vertex struct {
+	Lat, Long float64
+}
+
+func createMap(){
+	m := make(map[string]Vertex)
+	m["Bell Labs"] = Vertex{
+		40.68433, -74.39967,
+	}
+	fmt.Println(m["Bell Labs"])
+
+	var m2 = map[string]Vertex{
+		"Bell Labs": {40.68433, -74.39967},
+		"Google":    {37.42202, -122.08408},
+	}
+	fmt.Println(m2)
+}
+
+func mutateMap(){
+	m := make(map[string]int)
+
+	m["Answer"] = 42
+	fmt.Println("The value:", m["Answer"])
+
+	m["Answer"] = 48
+	fmt.Println("The value:", m["Answer"])
+
+	delete(m, "Answer")
+	fmt.Println("The value:", m["Answer"])
+
+	v, ok := m["Answer"]
+	fmt.Println("The value:", v, "Present?", ok)
+}
+
+
+func main(){
+	createMap()
+	mutateMap()
+}
